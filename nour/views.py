12defaf04a4286466f06b7a042dcff1d87ff07fe
rishitjs110805 <rishits1108@gmail.com>
2025-12-13 19:41:36 +0530

    Registration Updated

--- nour/views.py
+++ nour/views.py
@@ -4,26 +4,27 @@ from .models import *
 def index(request):
     return render(request, "index.html")
 
+# for ngo registration
 def register(request):
-        if request.method == "POST":
-            name = request.POST.get("username", "").strip()
-            email = request.POST.get("email", "").strip()
-            mobile = request.POST.get("mobile", "")
-            addr = request.POST.get("address", "")
-            pincode = request.POST.get("pincode", "")
-            pwd1 = request.POST.get("pass1", "")
-            pwd2 = request.POST.get("pass2", "")
-            if register_ngo.objects.filter(email=email).exists():
-                return redirect(loginpage)
+    if request.method == "POST":
+        name = request.POST.get("username", "").strip()
+        email = request.POST.get("email", "").strip()
+        mobile = request.POST.get("mobile", "")
+        addr = request.POST.get("address", "")
+        pincode = request.POST.get("pincode", "")
+        pwd1 = request.POST.get("pwd1", "")
+        pwd2 = request.POST.get("pwd2", "")
+        if register_ngo.objects.filter(email=email).exists():
+            return render(request, "login.html", {"error": "Account already exists. Please log in."})
+        else:
             if pwd1 != pwd2:
                 return render(request, "register_ngo.html", {"error": "Passwords do not match."})
+            else:
+                user = register_ngo(name=name, email=email,mobile=mobile,addr=addr,pincode=pincode, password=pwd1,timestamp="")
+                user.save()
+                return redirect(index)
 
-            user = register_ngo(name=name, email=email,mobile=mobile,addr=addr,pincode=pincode, password=pwd1,timestamp="")
-            user.save()
-            return redirect(index)
-
-        return render(request, "register_ngo.html")
-
+    return render(request, "register_ngo.html")
 
 def loginpage(request):
     return render(request, 'login.html')
