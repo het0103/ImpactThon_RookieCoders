7817b0962509f50b6845c9e95bc0a3405500c0a0
rishitjs110805 <rishits1108@gmail.com>
2025-12-17 18:25:49 +0530

    Changes

--- nour/urls.py
+++ nour/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index, name='index'),
+    path('',index),
     # path('about', about),
-    path('login', loginpage, name='loginpage'),
-    path('verifyuser', verifyuser, name='verifyuser'),
-    path('register_ngo',register, name='register_ngo'),
-    path('logout', logout, name='logout'),
-    path('donor-home', donor_home, name='donor_home'),
-    path('donor-demo', donor_demo, name='donor_demo'),
+    path('login', loginpage),
+    path('verifyuser', verifyuser),
+    path('register_ngo',register),
+    path('logout', logout),
+    path('donor-home', donor_home),
+    path('donor-demo', donor_demo),
 ]
