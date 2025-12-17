60e2ce3769c0ec37faeb38a407657ef9968da15d
rishitjs110805 <rishits1108@gmail.com>
2025-12-17 23:56:29 +0530

    About page attached

--- nour/urls.py
+++ nour/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('',index),
-    # path('about', about),
+    path('about', about),
     path('login', loginpage),
     path('verifyuser', verifyuser),
     path('register_ngo',register),
