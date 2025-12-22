2249eea10fb95731d1ae0b031be934fb74618a8b
Dakshu609 <dakshprajarati123@gmail.com>
2025-12-22 21:04:14 +0530

    commited

--- nour/static/assets/js/donor.js
+++ nour/static/assets/js/donor.js
@@ -221,7 +221,7 @@ class DonorApp {
                     </span>
                 </td>
                 <td>
-                    <button class="pickup-info-btn" onclick="window.open('pickup.html?id=${donation.id}', '_self')">View Info</button>
+                    <button class="pickup-info-btn" onclick="window.open('/pickup.html', '_self')" fdprocessedid="0n44nj">View Info</button>
                 </td>
             `;
             tbody.appendChild(row);
@@ -395,7 +395,7 @@ const pickupLocations = [
 ];
 
 function setupLocationAutocomplete() {
-    const locationInput = document.getElementById('pickup-location');
+    const locationInput = document.getElementById('pickuplocation');
     if (locationInput) {
         locationInput.addEventListener('input', function () {
             // Simple autocomplete implementation could go here
